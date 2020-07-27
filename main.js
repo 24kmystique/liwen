78517d0b3e3b2bd1f95ca793a6346e4fafc84678
24kmystique <56757916+24kmystique@users.noreply.github.com>
2020-07-27 12:38:39 +0800

    New Feature (Enlarge popup image) and Added Icons

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// var animation = bodymovin.loadAnimation( {
-// 	container: document.getElementById('homeAnim'),
-// 	rederer: 'svg',
-// 	autoplay: true,
-// 	path: './data.json'
-// })
-// var container = document.getElementById('homeAnim');
-
-// var animData = {
-// 	container: container,
-// 	renderer: 'svg',
-// 	autoplay: true,
-// 	loop: false,
-// 	path : './data.json'
-// };
-// var anim = bodymovin.loadAnimation(animData);
-
-
-
-
 var intViewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var intViewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 var homeHiContainer = document.getElementById('homeHiContainerId');
-// var homeHiContainerPos = homeHiContainer.getBoundingClientRect();
-// var homeHiContainerPosTop = homeHiContainerPos.top;
 
 
 window.onscroll = function() {
@@ -46,53 +12,34 @@ window.onscroll = function() {
 
 function changeHeader() {
 	// when scroll window, header background become solid
-
 	let headerBkg = document.getElementById('headerBkgId');
 	let navMenuContainerId = document.getElementById('navMenuContainerId');
 	let navSideMenuImageId = document.getElementById('navSideMenuImageId');
+	let MainImageContainer = document.querySelectorAll('.MainImageContainer');
 
 	// if main background image is out of view, nav header will have background
-
-	if ( (window.pageYOffset > intViewportHeight) && (intViewportWidth >= 768) ) { //changed homeHiContainerPosTop to intViewportHeight
-		headerBkg.classList.add('headerBkgAfter'); // not working
+	if ( ( (window.pageYOffset > intViewportHeight) || (window.pageYOffset > MainImageContainer[0].offsetHeight) ) && (intViewportWidth >= 768) ) {
+		headerBkg.classList.add('headerBkgAfter');
 		navSideMenuImageId.style.visibility = 'visible';
 		navMenuContainerId.style.visibility = 'hidden';
-		// alert('visibility works');
 	} else if (intViewportWidth >= 768) {
 		headerBkg.classList.remove('headerBkgAfter');
 		navSideMenuImageId.style.visibility = 'hidden';
 		navMenuContainerId.style.visibility = 'visible';
-	} else if ( (window.pageYOffset > intViewportHeight) && (intViewportWidth < 768) ) {
+	} else if ( ( (window.pageYOffset > intViewportHeight) || (window.pageYOffset > MainImageContainer[0].offsetHeight) ) && (intViewportWidth < 768) ) {
 		headerBkg.classList.add('headerBkgAfter');
 	} else if (intViewportWidth < 768) {
 		headerBkg.classList.remove('headerBkgAfter');
 	};
 
-	/*
-	if ( (window.pageYOffset > homeHiContainerPosTop) && (intViewportWidth >= 768) ) {
-		headerBkg.classList.add('headerBkgAfter'); // not working
-		navSideMenuImageId.style.visibility = 'visible';
-		navMenuContainerId.style.visibility = 'hidden';
-		// alert('visibility works');
-	} else if (intViewportWidth >= 768) {
-		headerBkg.classList.remove('headerBkgAfter');
-		navSideMenuImageId.style.visibility = 'hidden';
-		navMenuContainerId.style.visibility = 'visible';
-	} else if ( (window.pageYOffset > homeHiContainerPosTop) && (intViewportWidth < 768) ) {
-		headerBkg.classList.add('headerBkgAfter');
-	} else if (intViewportWidth < 768) {
-		headerBkg.classList.remove('headerBkgAfter');
-	}; */
-
-
 };
-
 
 
 var openSideNavTotalCount = 0;
 
 
 function openSideNav() {
+	// open side navigation bar
 	var sideNav = document.getElementById('navSideMenuOverlayContainerId');
 	let headerBkg = document.getElementById('headerBkgId');
 
@@ -107,7 +54,6 @@ function openSideNav() {
 		openSideNavTotalCount = 0;
 		openSideNav();
 	} else {
-		// headerBkg.classList.remove('headerBkgAfter');
 		openSideNavTotalCount = 0;
 		sideNav.style.width = '0%';
 	};
@@ -115,14 +61,32 @@ function openSideNav() {
 
 
 function projectPopup() {
+	// open popup above current frame
 	let projectPopupContainerId = document.getElementById('projectPopupContainerId');
 	projectPopupContainerId.style.visibility = 'visible';
+
+	// change popup text margin
+	let innerWidth = window.innerWidth;
+	let projectPopupText = document.querySelectorAll('.projectPopupText');
+	let projectPopupImage = document.getElementById('projectPopupImageContentId');
+	let style = getComputedStyle(projectPopupImage);
+	if (innerWidth <= 600) {
+		for (i=0; i<projectPopupText.length; i++) {
+			projectPopupText[i].style.marginLeft = style.marginLeft;
+			projectPopupText[i].style.marginRight = style.marginRight;
+		};
+		
+	};
 
 }
 
 function projectPopupExitButton() {
+	// close popup
 	let projectPopupContainerId = document.getElementById('projectPopupContainerId');
+	let projectPopupExit = document.getElementById('projectPopupExitImageContainerId');
+
 	projectPopupContainerId.style.visibility = 'hidden';
+	projectPopupExit.style.visibility = 'hidden';
 }
 
 
@@ -130,10 +94,9 @@ var slideIndex = 1;
 
 
 function changeProjectPopupImage(currentSlideIndex) {
-	
+	// popup slideshow
 	let numSlides = document.querySelectorAll('.projectPopupImage').length;
 	let projectPopupImage = document.getElementsByClassName('projectPopupImage');
-	//alert('currentSlideIndex: '+ currentSlideIndex);
 
 	if (currentSlideIndex > 0) {
 		slideIndex += currentSlideIndex;
@@ -146,23 +109,46 @@ function changeProjectPopupImage(currentSlideIndex) {
 	};
 
 	if ( (slideIndex <= 0) && (slideIndex > -1*numSlides + 1) ) {
-		// alert('if');
-		// alert('currentSlideIndex: ' + currentSlideIndex + 'slideIndex: ' + slideIndex);
 		projectPopupImage[numSlides + currentSlideIndex + slideIndex].style.display = 'flex';
 	} else if (slideIndex > numSlides || slideIndex <= -1*numSlides + 1) {
-		// alert('else if');
-		// alert('currentSlideIndex: ' + currentSlideIndex + 'slideIndex: ' + slideIndex);
 		projectPopupImage[0].style.display = 'flex';
 		slideIndex = 1;
 	} else {
-		// alert('else instead');
-		// alert('currentSlideIndex: ' + currentSlideIndex + 'slideIndex: ' + slideIndex);
 		projectPopupImage[slideIndex - 1].style.display = 'flex';
 	};
 
-}
+};
 
+function enlargeImage(currentImageIndex) {
+	// when image is clicked, enlarge image to full screen
+	let numImage = document.querySelectorAll('.projectPopupImage').length;
+	let projectEnlarge = document.querySelectorAll('.projectEnlargeImage');
+	let projectImage = document.querySelectorAll('.projectPopupImage');
+	let projectEnlargeContainer = document.getElementById('projectEnlargeImageContainerId');
+	let projectEnlargeBkg = document.getElementById('projectEnlargeBackgroundId');
+	let projectPopupExit = document.getElementById('projectPopupExitImageContainerId');
 
+	for (i=0; i<numImage; i++) {
+		if (i == currentImageIndex-1) {
+			// enlarge image
+			projectEnlargeBkg.style.visibility = 'visible';
+			projectEnlargeContainer.style.visibility = 'visible';
+			projectPopupExit.style.visibility = 'hidden';
+			projectEnlarge[0].src = projectImage[i].src;
+		};
+	};
+};
+
+function projectEnlargeExitButton() {
+	// close enlarged image
+	let projectEnlargeImageContainer = document.getElementById('projectEnlargeImageContainerId');
+	let projectEnlargeBackground = document.getElementById('projectEnlargeBackgroundId');
+	let projectPopupExit = document.getElementById('projectPopupExitImageContainerId');
+
+	projectEnlargeImageContainer.style.visibility = 'hidden';
+	projectEnlargeBackgroundId.style.visibility = 'hidden';
+	projectPopupExit.style.visibility = 'visible';
+};
 
 
 
